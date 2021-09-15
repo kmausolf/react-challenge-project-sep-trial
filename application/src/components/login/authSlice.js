b0790077f6bc69f4ee10c2f03c8994cb01671e74
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-09-15 14:51:37 -0700

    refactor(auth): move login behavior to helper function

--- application/src/components/login/authSlice.js
+++ application/src/components/login/authSlice.js
@@ -3,31 +3,30 @@ import { SERVER_IP } from "../../private";
 
 const INITIAL_STATE = { email: "", token: "" };
 
-export const login = createAsyncThunk(
-  "auth/loginStatus",
-  async (email, password, thunkAPI) => {
-    try {
-      fetch(`${SERVER_IP}/api/login`, {
-        method: "POST",
-        body: JSON.stringify({
-          email,
-          password,
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      })
-        .then((response) => response.json())
-        .then((response) => {
-          if (response.success) {
-            return { email: response.email, token: response.token };
-          } 
-        });
-    } catch (error) {
-      console.error(error);
-    }
+const performLogin = async (email, password, thunkAPI) => {
+  try {
+    fetch(`${SERVER_IP}/api/login`, {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        if (response.success) {
+          return { email: response.email, token: response.token };
+        }
+      });
+  } catch (error) {
+    console.error(error);
   }
-)
+};
+
+export const login = createAsyncThunk("auth/loginStatus", performLogin);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -41,8 +40,8 @@ export const authSlice = createSlice({
     builder.addCase(login.fulfilled, (state, action) => {
       state.email = action.email;
       state.token = action.token;
-    })
-  }
+    });
+  },
 });
 
 export const { logout } = authSlice.actions;
