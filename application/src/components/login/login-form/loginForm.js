272ac1834ad7607cd0d5fdce635e6f32dfbd769f
Kendrick Mausolf <kendrick.mausolf@gmail.com>
2021-09-16 10:53:53 -0700

    refactor(navigation): utilize promise chaining to navigate after login

--- application/src/components/login/login-form/loginForm.js
+++ application/src/components/login/login-form/loginForm.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useHistory } from 'react-router';
-import { login } from '../authSlice';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useHistory } from "react-router";
+import { login } from "../authSlice";
 
 const LoginForm = (props) => {
   const [email, setEmail] = useState("");
@@ -9,34 +9,49 @@ const LoginForm = (props) => {
 
   const dispatch = useDispatch();
   const history = useHistory();
-  const user = useSelector(state => state.auth);
-
-  useEffect(() => {
-    if(user?.token.length) {
-      history.push("/view-orders")
-    }
-  }, [user, history])
+  const user = useSelector((state) => state.auth);
 
   const handleLoginSubmit = (e) => {
     e.preventDefault();
-    dispatch(login(email, password));
-  }
+    dispatch(login(email, password)).then(() => {
+      history.push("/view-orders");
+    });
+  };
 
   return (
     <form>
       <div className="form-group">
         <label htmlFor="inputEmail">Email</label>
-        <input type="text" className="form-control" id="inputEmail" placeholder="test@test.com" value={email} onChange={e => setEmail(e.target.value)}></input>
+        <input
+          type="text"
+          className="form-control"
+          id="inputEmail"
+          placeholder="test@test.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></input>
       </div>
       <div className="form-group">
         <label htmlFor="inputPassword">Password</label>
-        <input type="password" className="form-control" id="inputPassword" value={password} onChange={e => setPassword(e.target.value)}></input>
+        <input
+          type="password"
+          className="form-control"
+          id="inputPassword"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        ></input>
       </div>
       <div className="d-flex justify-content-center">
-          <button onClick={handleLoginSubmit} type="submit" className="btn btn-primary">Login</button>
+        <button
+          onClick={handleLoginSubmit}
+          type="submit"
+          className="btn btn-primary"
+        >
+          Login
+        </button>
       </div>
     </form>
   );
-}
+};
 
 export default LoginForm;
--- application/src/components/nav/nav.js
+++ application/src/components/nav/nav.js
@@ -5,33 +5,33 @@ import { logout } from "../login/authSlice";
 import "./nav.css";
 
 const Nav = (props) => {
-    const dispatch = useDispatch();
-    const history = useHistory();
+  const dispatch = useDispatch();
+  const history = useHistory();
 
-    const onLogout = () => {
-        dispatch(logout());
-        history.push("/login");
-    }
+  const onLogout = () => {
+    dispatch(logout());
+    history.push("/login");
+  };
 
-    return (
-        <div className="nav-strip">
-            <Link to={"/order"} className="nav-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">Order Form</label>
-                </div>
-            </Link>
-            <Link to={"/view-orders"} className="nav-link" id="middle-link">
-                <div className="nav-link-style">
-                    <label className="nav-label">View Orders</label>
-                </div>
-            </Link>
-            <div className="nav-link" onClick={onLogout}>
-                <div className="nav-link-style">
-                    <label className="nav-label">Log Out</label>
-                </div>
-            </div>
+  return (
+    <div className="nav-strip">
+      <Link to={"/order"} className="nav-link">
+        <div className="nav-link-style">
+          <label className="nav-label">Order Form</label>
         </div>
-    );
-}
+      </Link>
+      <Link to={"/view-orders"} className="nav-link" id="middle-link">
+        <div className="nav-link-style">
+          <label className="nav-label">View Orders</label>
+        </div>
+      </Link>
+      <div className="nav-link" onClick={onLogout}>
+        <div className="nav-link-style">
+          <label className="nav-label">Log Out</label>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Nav;
